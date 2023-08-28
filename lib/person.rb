92c77da62eaab5058cbb2e195fb53c79d090c38f
iturres <emanueliturres.co@gmail.com>
2023-08-28 18:43:07 -0300

    fix: Rubocop offenses on 'person.rb' and 'student.rb'

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- lib/student.rb
+++ lib/student.rb
@@ -3,12 +3,12 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
 end
