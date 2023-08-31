aa88e3fa5645c340d945ec18b336b7fe81332f68
iturres <emanueliturres.co@gmail.com>
2023-08-30 21:32:10 -0300

    feat: on 'book.rb' 'rental.rb' and 'person.rb' Add intermediary:
    - Modify the 'Rental' class to intermediate between 'Person' and 'Book'.
      this allows to link people and books through rental transactions.

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,14 @@
 class Book
   attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(date)
+    @rentals << date
   end
 end
