26c2630b4059eace69075d3fb10fce091c4b5d03
iturres <emanueliturres.co@gmail.com>
2023-08-31 19:24:48 -0300

    Add 'dependencies.rb' to the project for:
    - loading all the necessary required files.
    - This will help avoiding repeating the require statements in every file.

---
+++ ui/dependencies.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/book'
+require_relative '../lib/rental'
+require_relative '../lib/person'
+require_relative '../lib/student'
+require_relative '../lib/teacher'
+require_relative '../helpers/get_name_and_age'
